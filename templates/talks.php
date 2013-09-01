dfe4b0efc076f7a780ad24b55c38b5944eaa2410
Alex Grande <grandecomplex@gmail.com>
2013-09-01 16:25:08 -0700

    Made schedule items designed

--- templates/talks.php
+++ templates/talks.php
@@ -28,9 +28,13 @@ require('./wp-blog-header.php');
       </div>
       <div class="span6">
         <h4>Forget exit strategy, think global improvement</h4>
+        <ul class="event-details clearfix">
+          <li class="datetime"><img alt="some_text" src=<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/cal.gif />October 10th</li>
+          <li><a target="_blank" href="https://maps.google.com/maps?q=1433+Market+St,+San+Francisco,+CA&hl=en&sll=37.7577,-122.4376&sspn=0.192446,0.277748&oq=1433+market+st.&hnear=1433+Market+St,+San+Francisco,+California+94102&t=m&z=16"><img alt="some_text" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/point.gif" />1433 Market St., San Francisco</a></li>
+        </ul>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <hr class="hr"></hr>
-        <h4>Speakers</h3>
+        <h4>Speakers</h4>
         <div class="circle-pic-list clearfix">      
           <div class="speaker" itemscope itemtype="http://schema.org/Person">
 
@@ -51,12 +55,8 @@ require('./wp-blog-header.php');
 
           </div>
         </div>
-        <hr class="hr"></hr>
-        <h4>Location</h4>
-        <a target="_blank" href="https://maps.google.com/maps?q=1433+Market+St,+San+Francisco,+CA&hl=en&sll=37.7577,-122.4376&sspn=0.192446,0.277748&oq=1433+market+st.&hnear=1433+Market+St,+San+Francisco,+California+94102&t=m&z=16">1433 Market St., San Francisco</a>
       </div>
       <div class="span2 offset1 centered">
-        <div class="datetime">OCT 10th</div>
         <a href="#" class="button button-primary button-large">Buy Tickets</a>
       </div>
     </div>
@@ -68,6 +68,157 @@ require('./wp-blog-header.php');
     <div class="centered">
       <h2 class="section_title">Event Schedule</h2>
     </div>
+    
+    <ul id="schedule">
+      <li>
+        <div class="schedule-item">
+          <div class="row-fluid">
+          <div class="span5">
+            <h3>Title of talk</h3>
+            <ul class="event-details clearfix">
+              <li class="datetime"><img alt="some_text" src=<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/cal.gif />October 10th</li>
+              <li><a target="_blank" href="https://maps.google.com/maps?q=1433+Market+St,+San+Francisco,+CA&hl=en&sll=37.7577,-122.4376&sspn=0.192446,0.277748&oq=1433+market+st.&hnear=1433+Market+St,+San+Francisco,+California+94102&t=m&z=16"><img alt="some_text" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/point.gif" />1433 Market St., San Francisco</a></li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore </p>
+          </div>
+          <div class="span5">
+            <h4>Speakers</h4>
+            <div class="circle-pic-list clearfix">      
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/vivek-wadhwa.jpg" />
+                <p>Vivek Wadhwa</p>
+
+              </div>
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/dave-mosby.jpg" />
+                <p>Dave Mosby</p>
+
+              </div>
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/Bill-Reichert.jpg" />
+                <p>Bill Reichert</p>
+
+              </div>
+            </div>
+          </div>
+          <div class="span2">
+            <a href="#" class="button">Buy tickets</a>
+          </div>
+        </div>
+        </div>
+        <div class="schedule-item"><div class="row-fluid">
+          <div class="span5">
+            <h3>Title of talk</h3>
+            <ul class="event-details clearfix">
+              <li class="datetime"><img alt="some_text" src=<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/cal.gif />October 10th</li>
+              <li><a target="_blank" href="https://maps.google.com/maps?q=1433+Market+St,+San+Francisco,+CA&hl=en&sll=37.7577,-122.4376&sspn=0.192446,0.277748&oq=1433+market+st.&hnear=1433+Market+St,+San+Francisco,+California+94102&t=m&z=16"><img alt="some_text" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/point.gif" />1433 Market St., San Francisco</a></li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore </p>
+          </div>
+          <div class="span5">
+            <h4>Speakers</h4>
+            <div class="circle-pic-list clearfix">      
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/vivek-wadhwa.jpg" />
+                <p>Vivek Wadhwa</p>
+
+              </div>
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/dave-mosby.jpg" />
+                <p>Dave Mosby</p>
+
+              </div>
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/Bill-Reichert.jpg" />
+                <p>Bill Reichert</p>
+
+              </div>
+            </div>
+          </div>
+          <div class="span2">
+            <a href="#" class="button">Buy tickets</a>
+          </div>
+        </div></div>
+        <div class="schedule-item"><div class="row-fluid">
+          <div class="span5">
+            <h3>Title of talk</h3>
+            <ul class="event-details clearfix">
+              <li class="datetime"><img alt="some_text" src=<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/cal.gif />October 10th</li>
+              <li><a target="_blank" href="https://maps.google.com/maps?q=1433+Market+St,+San+Francisco,+CA&hl=en&sll=37.7577,-122.4376&sspn=0.192446,0.277748&oq=1433+market+st.&hnear=1433+Market+St,+San+Francisco,+California+94102&t=m&z=16"><img alt="some_text" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/point.gif" />1433 Market St., San Francisco</a></li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore </p>
+          </div>
+          <div class="span5">
+            <h4>Speakers</h4>
+            <div class="circle-pic-list clearfix">      
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/vivek-wadhwa.jpg" />
+                <p>Vivek Wadhwa</p>
+
+              </div>
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/dave-mosby.jpg" />
+                <p>Dave Mosby</p>
+
+              </div>
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/Bill-Reichert.jpg" />
+                <p>Bill Reichert</p>
+
+              </div>
+            </div>
+          </div>
+          <div class="span2">
+            <a href="#" class="button">Buy tickets</a>
+          </div>
+        </div></div>
+        <div class="schedule-item"><div class="row-fluid">
+          <div class="span5">
+            <h3>Title of talk</h3>
+            <ul class="event-details clearfix">
+              <li class="datetime"><img alt="some_text" src=<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/cal.gif />October 10th</li>
+              <li><a target="_blank" href="https://maps.google.com/maps?q=1433+Market+St,+San+Francisco,+CA&hl=en&sll=37.7577,-122.4376&sspn=0.192446,0.277748&oq=1433+market+st.&hnear=1433+Market+St,+San+Francisco,+California+94102&t=m&z=16"><img alt="some_text" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/icons/point.gif" />1433 Market St., San Francisco</a></li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore </p>
+          </div>
+          <div class="span5">
+            <h4>Speakers</h4>
+            <div class="circle-pic-list clearfix">      
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/vivek-wadhwa.jpg" />
+                <p>Vivek Wadhwa</p>
+
+              </div>
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/dave-mosby.jpg" />
+                <p>Dave Mosby</p>
+
+              </div>
+              <div class="speaker" itemscope itemtype="http://schema.org/Person">
+
+                <img class="circle_pic" alt="" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/pages/homepage/speakers/Bill-Reichert.jpg" />
+                <p>Bill Reichert</p>
+
+              </div>
+            </div>
+          </div>
+          <div class="span2">
+            <a href="#" class="button">Buy tickets</a>
+          </div>
+        </div></div>
+      </li>
+    </ul>
   </div>
   
   <div id="section3" class="section clearfix centered">
