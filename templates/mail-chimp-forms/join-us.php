076c4ff23bf2aa97ed51a8687b69d6d1e140d3f0
Alex Grande <grandecomplex@gmail.com>
2013-09-03 13:39:23 -0700

    Moving forms to wufoo

--- templates/mail-chimp-forms/join-us.php
+++ templates/mail-chimp-forms/join-us.php
@@ -1,38 +1,17 @@
-
-<div id="mc_embed_signup" class="mc-form">
-  <div class="top">
-    <h2>Join us</h2>
-    <p>We are looking for volunteers, speakers, and sponsors.</p>
-    <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-  </div>
-<form action="http://hackforbigchoices.us7.list-manage.com/subscribe/post?u=84324a24b8bb798baeadac59e&amp;id=f34e2daf34" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+<div id="wufoo-m7x4z5">
+Fill out my <a href="http://planetio.wufoo.com/forms/m7x4z5">online form</a>.
 </div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">Full name </label>
-	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-</div>
-<div class="mc-field-group">
-	<label for="mce-MMERGE3">Reason </label>
-	<select name="MMERGE3" class="" id="mce-MMERGE3">
-	<option value=""></option>
-	<option value="Sponsor">Sponsor</option>
-<option value="Present">Present</option>
-<option value="Volunteer">Volunteer</option>
-<option value="Hack">Hack</option>
-
-	</select>
-</div>
-
-
-
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>	
-  <div class="button-wrapper"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button button-primary"></div>
-</form>
-</div>
+<script type="text/javascript">var m7x4z5;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'planetio', 
+'formHash':'m7x4z5', 
+'autoResize':true,
+'height':'400',
+'async':true,
+'header':'show'};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { m7x4z5 = new WufooForm();m7x4z5.initialize(options);m7x4z5.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');</script>
