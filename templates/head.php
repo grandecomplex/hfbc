e4375a11cc69d3b75251bfd474655f27eda689a1
Alex Grande <grandecomplex@gmail.com>
2013-09-08 22:18:53 -0700

    Adding favicon

--- templates/head.php
+++ templates/head.php
@@ -17,6 +17,10 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
+  <link rel="icon" 
+        type="image/png" 
+        href="<?= $favicon ?>">
+  
   <meta property="og:title" content="<?= $title ?>"> 
 
   <meta property="og:description" content="<?= $description ?>"> 
