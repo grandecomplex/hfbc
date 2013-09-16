0be67d2f2f2c783028050e4bded31f17fd9b4700
Alex Grande <grandecomplex@gmail.com>
2013-09-16 15:32:02 -0700

    Updated opening ceremony form

--- templates/request-invite-open-ceremony.php
+++ templates/request-invite-open-ceremony.php
@@ -22,7 +22,7 @@ $(function() {
   $("#join-us-overlay").remove();
 });
 </script>
-
+ 
 <!-- Begin MailChimp Signup Form -->
 <style type="text/css">
 	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
