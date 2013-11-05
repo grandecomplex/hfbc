679d726a65c2b7461e0bb319a11d6b563be0d26f
Alex Grande <grandecomplex@gmail.com>
2013-11-04 16:49:01 -0800

    Added copy

--- templates/footer.php
+++ templates/footer.php
@@ -33,5 +33,6 @@
 <?php wp_footer(); ?>
 
 <div id="join-us-overlay" class="hidden mc_form overlay-window">
+  <p>Do you want to join our global community of leaders and change makers? We will keep you updated about our next move and give you exclusive access.</p>
   <iframe src="/join-us-form"></iframe>
 </div>
